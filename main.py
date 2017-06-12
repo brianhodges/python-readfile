4f9f44ae7eef7da80b33c0fd581f89b2f889cad0
Development <Development@Brian-N-Hodgess-MacBook.local>
2017-06-12 01:36:59 -0400

    updated for python 3.6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from os import path
 from datetime import datetime
 from team import Team
-files = filter(path.isfile, os.listdir('./'))
+files = list(filter(path.isfile, os.listdir('./')))
 filename = 'top3list.csv'
 delim = '+'
 x = 0
@@ -13,8 +13,8 @@ teams = []
 print
 while x < len(files):
 	if files[x] == filename:
-		print 'Filename: ' + files[x]
-		print 'Modified At: ' + datetime.fromtimestamp(os.path.getctime('./' + files[x])).strftime('%Y-%m-%d %H:%M %p')
+		print('Filename: ' + files[x])
+		print('Modified At: ' + datetime.fromtimestamp(os.path.getctime('./' + files[x])).strftime('%Y-%m-%d %H:%M %p'))
 	x += 1
 
 with open(filename) as f:
@@ -30,10 +30,10 @@ print
 
 while z < len(teams):
 	s = len('| Team: ' + teams[z].name + ' |') - 2
-	print delim + ('-' * s) + delim
-	print '| Team: ' + teams[z].name + ' |'
+	print(delim + ('-' * s) + delim)
+	print('| Team: ' + teams[z].name + ' |')
 	s2 = len('| Super Bowl Wins: ' + teams[z].super_bowl_wins)
-	print '| Super Bowl Wins: ' + teams[z].super_bowl_wins + (' ' * (s - s2)) + ' |'
-	print delim + ('-' * s) + delim
+	print('| Super Bowl Wins: ' + teams[z].super_bowl_wins + (' ' * (s - s2)) + ' |')
+	print(delim + ('-' * s) + delim)
 	print
 	z += 1
